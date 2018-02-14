46ae7a32f6675f1eb5c4aaf34a49f023a1853527
Joanna Wasiluk <jmwasiluk@yahoo.com>
2018-02-14 15:24:48 +0000

    Added profile create action

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -8,6 +8,32 @@
           @profiles = Profile.order('created_at DESC')
           render json: {status: 'SUCCESS', message: 'Loaded Witches & Wizards', data:@profiles}, status: :ok
         end  
+        
+        # GET to /profiles/1
+        # GET to /profiles/1.json
+        def show
+          @profile = Profile.find(params[:id])
+          render json: {status: 'SUCCESS', message: 'Loaded Witch or Wizard', data:@profile}, status: :ok
+        end
+        
+        # POST to /profiles
+        # POST to /profiles.json
+        def create
+          @profile = Profile.new(profile_params)
+          
+          if @profile.save
+            render json: {status: 'SUCCESS', message: 'Created Witch/Wizard', data:@profile}, status: :created
+          else 
+            render json: {status: 'ERROR', message: 'Witch/Wizard not created', data:@profile.errors}, status: :unprocessable_entity
+          end
+        end
+        
+        
+        private
+        # Never trust parameters from the "scary" internet, only allow the white list through
+        def profile_params
+          params.permit(:name, :location, :body)
+        end
       end
     end  
   end
